8424f32965b984cf7d08ce364756e2ea8a5cf408
Ayush Thakur <ayush26819@gmail.com>
2023-11-26 11:37:55 +0530

    Commit for spelling wrong

--- src/main/java/com/dailycodebuffer/ProductService/ProductServiceApplication.java
+++ src/main/java/com/dailycodebuffer/ProductService/ProductServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductServoceApplication {
+public class ProductServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductServoceApplication.class, args);
+		SpringApplication.run(ProductServiceApplication.class, args);
 	}
 
 }
